5655ee808c75a910694e66fda377a02df0fb348d
Oscar <oscarquanla@gmail.com>
2022-08-13 12:24:07 +0700

    Autowire Demo

---
+++ src/main/java/com/example/entities/Person.java
@@ -0,0 +1,11 @@
+package com.example.entities;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+	@Autowired
+	private Person person;
+	
+}
